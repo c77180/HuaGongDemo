4d50c87668bfd55d7acebd2574c7ce532adc4a4d
xiaoDingc <771801383@qq.com>
2018-04-16 23:54:01 +0800

    完成8-9产品前台页面布置

--- 化工站/ProductList.ashx.cs
+++ 化工站/ProductList.ashx.cs
@@ -23,6 +23,7 @@ namespace 化工站
                 pageNum=Convert.ToInt32(context.Request["pageNum"]);
 
             }
+            int pre = pageNum - 1;
             DataTable products = SqlHelper.ExecuteDataTable("select * from (select *,row_number() over(order by p.Id asc) as  num  from T_Products p ) s where s.num between @Start and @End",
             new SqlParameter("@Start", (pageNum - 1) * 9 + 1), new SqlParameter("@End", pageNum * 9));
             int totalCount=(int)SqlHelper.ExecuteScalar("select count(*) from T_Products");
@@ -30,10 +31,10 @@ namespace 化工站
             object[] pageData=new object[pageCount];
            for (int i = 0; i < pageData.Length; i++)
 			{
-			    pageData[i]=new {Href="ProductList.ashx?PageNum="+(i+1),Title="第"+(i+1)+"页"};
+			    pageData[i]=new {Href="ProductList.ashx?PageNum="+(i+1),Title=(i+1)};
 			}
 
-           var data = new { Products = products.Rows, PageData = pageData, totalCount = totalCount, PageNum = pageNum, PageCount=pageCount};
+           var data = new { Products = products.Rows, PageData = pageData, totalCount = totalCount,Pre= pre, PageNum = pageNum, PageCount=pageCount};
             string html = CommonHelper.RenderHtml("Front/ProductList.html", data);
             context.Response.Write(html);
         }
